2eb6384dbf705a36080b1f0802c0b0c9bbf8f6d5
MyUsernme <gabriele.morelli@live.it>
2022-03-14 14:23:03 +0100

    esercizio

--- index.php
+++ index.php
@@ -1,10 +1,8 @@
-
-
 <?php
 
-$Faq = [
+$Faqs = [
 
-    'Faq1' => [
+   [
         'domanda' => 'Come state implementando la recente decisione della Corte di giustizia dell Unione europea  relativa al diritto all oblio?',
         'risposta' => 'La recente decisione della Corte di giustizia dell Unione europea ha profonde conseguenze per i motori di ricerca in Europa. La Corte ha stabilito che alcuni utenti hanno il diritto di chiedere ai motori di ricerca come Google di rimuovere risultati relativi a chiavi di ricerca che includono il proprio nome. Per poter essere rimossi, i risultati visualizzati devono essere inadeguati, irrilevanti o non più rilevanti, o eccessivi<br><br>
         Da quando questa decisione è stata pubblicata il 13 maggio 2014, abbiamo lavorato incessantemente per uniformarci a essa. Si tratta di una procedura complessa perché dobbiamo valutare ogni singola richiesta e effettuare un bilanciamento tra il diritto dell individuo a controllare i suoi dati personali ed il dritto di tutti di conoscere e distribuire le informazioni.<br><br>per presentare una richiesta di rimozione, compila questo modulo web. 
@@ -15,13 +13,13 @@ $Faq = [
     ],
     
     
-    'Faq2' => [
+     [
         'domanda' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
         'risposta' => 'Sappiamo che la sicurezza e la privacy sono importanti per te e lo sono anche per noi. Per noi è imperativo offrire un elevata sicurezza e assicurarti che le tue informazioni sono al sicuro e accessibili quando ne hai bisogno.<br><br>Siamo costantemente al lavoro per garantire un elevata sicurezza, proteggere la tua privacy e rendere Google ancora più efficace ed efficiente per te. Spendiamo centinaia di milioni di dollari ogni anno per la sicurezza e ci avvaliamo di esperti di fama mondiale in materia di sicurezza dei dati per mantenere le tue informazioni al sicuro. Abbiamo inoltre sviluppato strumenti per la sicurezza e la privacy di facile utilizzo come Google Dashboard, la verifica in due passaggi e Impostazioni annunci. Così, per quanto riguarda le informazioni che condividi con Google, hai il massimo controllo.<br><br>Puoi leggere ulteriori informazioni sulla sicurezza online, incluso come proteggere te e la tua famiglia online, nel Centro Google per la sicurezza online.<br><br>Scopri quali misure adottiamo per garantire protezione e sicurezza alle tue informazioni personali, lasciando a te il controllo.',
     ],
     
     
-    'Faq3' => [
+    [
         'domanda' => 'Perché il mio account è associato a un paese?',
         'risposta' => 'Il tuo account è associato a un paese (o territorio) nei Termini di servizio per poter stabilire due cose:<br><br>
 
@@ -31,18 +29,18 @@ $Faq = [
         Tieni presente che i servizi Google sono fondamentalmente gli stessi a prescindere dalla società consociata che li offre o dal paese a cui è associato il tuo account.',
     ],
     
-    'Faq4' => [
+     [
         'domanda' => 'Stabilire il paese associato al tuo account',
         'risposta' => 'Quando crei un nuovo Account Google, lo associamo a un paese in base a dove è stato creato. Per quanto riguarda gli account creati almeno un anno fa, usiamo il paese da cui accedi solitamente ai servizi Google, in genere i servizi in cui hai trascorso più tempo nellultimo anno.<br><br>
         I viaggi frequenti solitamente non influiscono sul paese associato al tuo account. Se ti trasferisci in un altro paese, potrebbe occorrere circa un anno per aggiornare l associazione del paese.<br><br>Se il paese associato al tuo account non corrisponde al tuo paese di residenza, il motivo potrebbe essere la differenza tra il paese in cui lavori e il paese in cui risiedi, linstallazione di una rete privata virtuale (VPN) per mascherare il tuo indirizzo IP oppure la residenza vicino a un confine territoriale. Contattaci se ritieni che il paese associato al tuo account sia sbagliato.',
     ],
     
-    'Faq5' => [
+    [
         'domanda' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
         'risposta' => 'I risultati di ricerca di Google rispecchiano i contenuti pubblicamente disponibili sul Web. I motori di ricerca non possono rimuovere i contenuti direttamente dai siti web, quindi rimuovere risultati di ricerca da Google non consente di rimuovere i contenuti dal Web. Se desideri rimuovere qualcosa dal Web, devi contattare il webmaster del sito su cui sono pubblicati i contenuti e chiedergli di apportare una modifica. Inoltre, se, ai sensi delle leggi europee per la protezione dei dati, desideri richiedere la rimozione di determinate informazioni su di te visualizzate nei risultati di ricerca di Google, fai clic qui. Una volta che i contenuti saranno stati rimossi e che Google avrà rilevato l aggiornamento, le informazioni non verranno più visualizzate nei risultati di ricerca di Google. In caso di una richiesta di rimozione urgente, è inoltre possibile visitare la nostra pagina di assistenza per avere ulteriori informazioni.',
     ],
     
-    'Faq6'  => [
+    [
         'domanda' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
         'risposta' => 'In alcuni casi sì. Quando fai clic su un risultato della Ricerca Google, il tuo browser web potrebbe reindirizzare alla pagina web di destinazione anche l indirizzo Internet, o URL, della pagina dei risultati di ricerca sotto forma di URL referrer. Talvolta, l URL della pagina dei risultati di ricerca potrebbe contenere la query di ricerca che hai inserito. Se utilizzi la ricerca SSL (la funzione di ricerca criptata di Google), nella maggior parte dei casi i termini di ricerca non vengono inviati come parte dell URL negli URL referrer. Questo comportamento può fare eccezione, ad esempio se utilizzi alcuni browser meno diffusi. Ulteriori informazioni sulla ricerca SSL sono disponibili qui. Le query di ricerca o le informazioni contenute nell URL referrer potrebbero essere disponibili mediante Google Analytics o un API (Application Programming Interface). Inoltre, gli inserzionisti potrebbero ricevere informazioni relative all esatte parole chiave che hanno determinato il clic su un annuncio.',
     ],
@@ -52,29 +50,9 @@ $Faq = [
 
 
 
-foreach ($Faq as $key => $valor){
-
-        echo"<h1> $key </h1>";
-
-    foreach($valor as $content){
-        
-        echo "<h2> $content  </h2>";
-        
-     
-    }
-
-
-
-
-
-
-
-
-}
 
 ?>
 
-<!--
 
 <!DOCTYPE html>
 <html lang="en">
@@ -83,30 +61,46 @@ foreach ($Faq as $key => $valor){
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel='stylesheet' href="style.css">
 </head>
 <body>
     <header>
         <div class ="nav-top">
-            <div class ="title">
-                <ul>
-                    <li>google</li>
-                    <li>privacy e termini</li>
-                </ul>
-            </div>
-            <div class="button">
-                <button>Accedi</button>
-            </div>
+        <div class="logo">
+            logo
+        </div> 
+        <div class="title"> Privacy e Termini</div>
         </div>
-        <div class = "nav-bottom">
-            <ul>
-                <li>introduzione</li>
-                <li>norme sulla privacy</li>
-                <li>tecnologie</li>
-                <li>Domande frequenti</li>
+        <div class ="nav-bottom">
+         <ul>
+            <li> <a href="#">introduzione</a></li>
+            <li> <a href="#">norme sulla privacy</a></li>
+            <li> <a href="#">tecnologie</a></li>
+            <li class="actvive"><a href="#"> Domande frequenti</a></li>
             </ul>
         </div>
     </header>
+
+    <main>
+        <div class="container">
+        <?php
+        
+    foreach ($Faqs as $Faq) {
+       
+        echo '<h2>' . $Faq['domanda'] . '</h2>';
+        echo '<p>' . $Faq['risposta'] . '</p>';
+
+    }
+
+         ?>
+        </div>
+   
+
+      
+
+
+    
+    </main>
 </body>
 </html>
 
